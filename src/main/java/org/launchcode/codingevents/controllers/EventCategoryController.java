fdffeb8572932a74001338fb574314dfd9c484af
sreavis07 <sean_reavis@cable.comcast.com>
2021-07-27 17:57:44 -0400

    exercises completed and corrected

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -21,14 +21,14 @@ public class EventCategoryController {
     private EventCategoryRepository eventCategoryRepository;
 
     @GetMapping
-    public String displayAllEvents(Model model) {
+    public String displayAllCategories(Model model) {
         model.addAttribute( "title", "All Categories" );
         model.addAttribute( "categories", eventCategoryRepository.findAll() );
         return "eventCategories/index";
     }
 
     @GetMapping("create")
-    public String displayAllCategories(Model model) {
+    public String renderCreateEventCategoryForm(Model model) {
         model.addAttribute( "title", "Create Category" );
         model.addAttribute( new EventCategory() );
         return "eventCategories/create";
